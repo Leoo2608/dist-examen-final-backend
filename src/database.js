40cc6f599c2b5d5766e595d1ea371290058c972a
Leoo2608 <leonardo.antazu@upeu.edu.pe>
2022-07-17 15:31:15 -0500

    database.js credentails updated

--- src/database.js
+++ src/database.js
@@ -1,9 +1,11 @@
 import { Pool } from 'pg'
+
+
 export const pool=new Pool({
-    host:'ec2-54-147-93-73.compute-1.amazonaws.com',
-    user:'hiloviqepdzfhw',
-    password:'f4be3fbfa5c340b9745721209f16091f6e735ae98127af98a0c6cdbf52df5943',
-    database:'ddi4u2ubl2du8t',
+    host:'database-1.caht6zftdihk.us-east-1.rds.amazonaws.com',
+    user:'postgres',
+    password:'12345678',
+    database:'dist',
     port:5432,
     ssl:{rejectUnauthorized:false}
 });
